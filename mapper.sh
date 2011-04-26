9996de6600df1f848d29c8b9b63a3adb10789881
Sam Neubardt <samneubardt@gmail.com>
2011-04-26 14:46:25 -0400

    Remove debug echos

--- mapper.sh
+++ mapper.sh
@@ -11,11 +11,9 @@ mkdir $TEMP_DIR
 for ext in "${SUFFIXES[@]}"
 do
     filename=$IN_FILE$ext
-    echo hadoop fs -get $filename $TEMP_DIR/ 1>&2
     hadoop fs -get $filename $TEMP_DIR/
 done
 
-echo $PROGRAM $TEMP_DIR/${IN_FILE##*/}.shx $OUT_FILE 1>&2
 $PROGRAM $TEMP_DIR/${IN_FILE##*/}.shx $OUT_FILE
 
 cat $OUT_FILE.txt
