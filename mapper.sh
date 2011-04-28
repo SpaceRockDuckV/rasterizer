ebdb0ec0f00015fdfe7a930dc5f6740bd235269a
Sam Neubardt <samneubardt@gmail.com>
2011-04-28 16:25:54 -0400

    No longer need to store the output of quadtree in a temporary file since it writes directly to STDOUT

--- mapper.sh
+++ mapper.sh
@@ -3,7 +3,6 @@ read IN
 IN_FILE=/${IN#*/}
 
 TEMP_DIR=./tmp
-OUT_FILE=$TEMP_DIR/output
 SUFFIXES=(.shp .shx)
 PROGRAM=./quadtree 
 
@@ -14,8 +13,6 @@ do
     hadoop fs -get $filename $TEMP_DIR/
 done
 
-$PROGRAM $TEMP_DIR/${IN_FILE##*/}.shx $OUT_FILE
-
-cat $OUT_FILE.txt
+$PROGRAM $TEMP_DIR/${IN_FILE##*/}.shx
 
 rm -rf $TEMP_DIR
